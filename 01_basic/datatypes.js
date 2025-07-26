55e5077fadc9cce434bc1d492130dd238f803c70
Shankar Kumar <118601944+shankarsinghania@users.noreply.github.com>
2025-07-26 10:36:43 +0000

    datatypes

---
+++ 01_basic/datatypes.js
@@ -0,0 +1,24 @@
+console.log("hello");
+console.log('hello');
+
+let age=11
+
+console.log(typeof age);
+
+let name1='shankar'
+console.log(typeof name1);
+
+let IsloggedIn=false;
+console.log(typeof IsloggedIn);
+
+let state;
+console.log(typeof state);
+
+let number=null
+console.log(typeof number);
+
+
+let num=undefined;
+console.log(typeof num);
+
+
---
+++ 01_basic/first.js
@@ -0,0 +1,9 @@
+const accountId =1234
+let accountEmail="hello@gmail.com"
+var accountPassword="1234"
+accountCity="Bihar"
+
+console.log(accountId);
+
+console.table([accountId,accountEmail,accountPassword,accountCity])
+ 
